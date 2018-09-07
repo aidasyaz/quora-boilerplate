b496923358414c11f8b4941a36364d862a59bbd2
aidasyaz <aida.syaz24@gmail.com>
2018-09-07 16:28:57 +0800

    Added new routes to controller

--- app.rb
+++ app.rb
@@ -1,7 +1,104 @@
+##controller actions in here
+
 require_relative './config/init.rb'
 set :run, true
 
 get '/' do
   @name = "Bob Smith"
-  erb :"home"
+  erb :"home" #erb > embedded ruby to render 
 end
+
+# get '/date' do #will go to link
+# 	@date = Time.now
+# 	erb :"date/time" 
+# end
+
+get '/signup' do
+	erb :"/signup"
+end
+
+post '/signup' do
+	
+	user = User.new(params[:user])
+	if user.save
+		#redirect to login page
+		redirect :"/"
+	else
+		#redirect to signup page
+		redirect :"/sign_up"
+	end
+end
+
+get '/login' do
+	erb :"/login"
+end
+
+post '/login' do
+	# 1. check if email exist
+	@user = User.find_by(email: params[:email])
+
+	# 2. check if can find user and if password is valid
+	# (valid-profile page) (invalid- relogin)
+	if @user && @user.authenticate(params[:password])
+		sign_in(user) #from session in helper
+		redirect user_path (@user)
+
+	else
+		redirect '/login'
+	end
+
+  	erb :"/login"
+end
+
+
+get '/users/:id' do
+	#find user by id
+	@user = User.find_by(id: params[:id])
+	#if id exist,redirect to profile page
+	
+	if @user
+		erb :'users/profile'
+	else
+		redirect '/404'
+	end
+end
+
+get '/404' do
+	return "something went wrong :("
+end
+
+get'/logout' do
+  sign_out # uses the sign out session in helpers file
+  # redirect
+  redirect back
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
